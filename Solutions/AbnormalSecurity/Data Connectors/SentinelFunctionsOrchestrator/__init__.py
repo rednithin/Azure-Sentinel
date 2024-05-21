f644e3870a566dc4c766e6158925af0be04f5873
Nithin Reddy <nreddy@abnormalsecurity.com>
2024-05-21 15:11:06 +0000

    hardcoded dates

--- Solutions/AbnormalSecurity/Data Connectors/SentinelFunctionsOrchestrator/__init__.py
+++ Solutions/AbnormalSecurity/Data Connectors/SentinelFunctionsOrchestrator/__init__.py
@@ -52,7 +52,8 @@ def should_reset_date_params():
     return RESET_ORCHESTRATION == "true"
 
 async def transfer_abnormal_data_to_sentinel(stored_threats_datetime,stored_cases_datetime, current_datetime, context):
-    threats_date_filter = {"gte_datetime": stored_threats_datetime, "lte_datetime": current_datetime}
+
+    threats_date_filter = {"gte_datetime": "2024-05-07T08:10:00Z", "lte_datetime": "2024-05-07T08:15:00Z"}
     cases_date_filter = {"gte_datetime": stored_cases_datetime, "lte_datetime": current_datetime}
     queue = asyncio.Queue()
     api_connector = AbnormalSoarConnectorAsync(API_TOKEN)
