e205c95c984cda066e188258ec6d8649def33868
Nithin Reddy <nreddy@abnormalsecurity.com>
2024-09-26 13:09:49 +0000

    headers trace id

--- Solutions/AbnormalSecurity/Data Connectors/SentinelFunctionsOrchestrator/soar_connector_async_v2.py
+++ Solutions/AbnormalSecurity/Data Connectors/SentinelFunctionsOrchestrator/soar_connector_async_v2.py
@@ -41,6 +41,7 @@ def get_query_params(
 
 def get_headers(ctx: Context) -> Dict[str, str]:
     return {
+        "X-Abnormal-Trace-Id": str(ctx.TRACE_ID),
         "Authorization": f"Bearer {ctx.API_TOKEN}",
         "Soar-Integration-Origin": "AZURE SENTINEL",
         "Azure-Sentinel-Version": "2024-09-15",
